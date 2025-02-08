ff17423de293ecbe692167b994ffe9a99084355a
Haris Khan <developewithharis@gmail.com>
2025-02-08 16:08:42 +0500

    efficient

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import { processFile } from "./main.js"; // AI agent function
 
 configDotenv();
 
-const allowedOrigins = process.env.ORIGIN;
+const allowedOrigins = process.env.NEXT_PUBLIC_ORIGIN;
 
 const app = express();
 app.use(cors({ 
